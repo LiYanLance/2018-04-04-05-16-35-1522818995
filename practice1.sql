8fb78a1ab52116fe07f0b67c7787d38ecf01ff23
LiYanLance <leeyanly@163.com>
2018-05-04 11:20:39 +0800

    SQL finish practice1

--- practice1.sql
+++ practice1.sql
@@ -11,3 +11,22 @@
 # 6.找出Employee表中姓名包含`n`字符并且薪资大于6000的雇员所有个人信息
 
 # ps:本练习中只需要将6的结果复制到`result.txt`文件中
+
+
+create table Employee (id int, name varchar(20), age int, gender varchar(6), companyId int, salary decimal(10,2));
+
+create table Company (id int, companyName varchar(20), employeesNumber int);
+
+load data infile 'C:/Program Files/MySQL/mysql-files/employee-data.csv'
+into table Employee
+fields terminated by ','  optionally enclosed by '\'' escaped by '"'
+lines terminated by '\r\n';
+
+# 如果有权限问题：https://stackoverflow.com/questions/31951468/error-code-1290-the-mysql-server-is-running-with-the-secure-file-priv-option
+
+load data infile 'C:/Program Files/MySQL/mysql-files/company-data.csv'
+into table Company
+fields terminated by ','  optionally enclosed by '\'' escaped by '"'
+lines terminated by '\r\n';
+
+select * from employee where name like "%n%" and salary > 6000;
