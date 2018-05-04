170b58babd475b6c04694551cdfe8e145d11b16c
LiYanLance <leeyanly@163.com>
2018-05-04 11:27:02 +0800

    SQL finish practice3

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,8 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+SELECT companyName,cast(Avg(salary) as decimal(10, 2)) as avgSalary
+FROM company,employee
+WHERE company.id = employee.companyId
+GROUP BY companyId;
 
